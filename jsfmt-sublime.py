a898fde190e6c6ebe23fb46f3609e95c16e1203b
Paul Irish <paul.irish@gmail.com>
2014-09-25 14:08:23 -0700

    updating plugin to modern jsfmt global

--- jsfmt-sublime.py
+++ jsfmt-sublime.py
@@ -2,9 +2,9 @@ import subprocess
 import sublime, sublime_plugin
 import os
 
-PLUGIN_FOLDER = os.path.dirname(os.path.realpath(__file__))
-SCRIPT_PATH = PLUGIN_FOLDER + '/node_modules/jsfmt/run.js'
-NODE_PATH = '/usr/local/bin/node'
+# jsfmt must be installed as a global npm module
+BINARY_PATH = 'jsfmt'
+
 
 class FormatJavascript(sublime_plugin.TextCommand):
   def run(self, edit):
@@ -13,10 +13,7 @@ class FormatJavascript(sublime_plugin.TextCommand):
         region = sublime.Region(0, self.view.size())
         content = self.view.substr(region)
 
-        cmd = [NODE_PATH, SCRIPT_PATH, '--format']
-        args = '"' + '" "'.join(cmd) + '"'
-
-        p = subprocess.Popen(args, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(BINARY_PATH, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = p.communicate(content.encode('utf-8'))
 
         if stderr:
