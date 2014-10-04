42e17ab08b1ddf56d496c96bf9a1e56bcd525342
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2014-10-04 13:29:06 +0300

    Fixes the settings bug #15

--- jsfmt.js
+++ jsfmt.js
@@ -1,19 +1,22 @@
 'use strict';
 var stdin = require('get-stdin');
 var jsfmt = require('jsfmt');
+var extend = require('extend');
+
 
 stdin(function(data) {
     // deep extend
     var scope = process.argv[3];
     var conf = jsfmt.getConfig();
-    var optsJSON = extend(true, conf, JSON.parse(process.argv[4]));
+
+    var optsJSON = extend(true, {}, conf, JSON.parse(process.argv[4]));
     var opts = conf;
     var js;
 
     // if we don't have a jsfmtrc file
     // use the settings from .sublime-settings
-    if( ! conf.config) {
-        opts = extend(true, conf, JSON.parse(process.argv[2]));
+    if( ! opts.config) {
+        opts = extend(true, {}, opts, JSON.parse(process.argv[2]));
     }
 
     try {
@@ -29,95 +32,3 @@ stdin(function(data) {
     }
 });
 
-// http://stackoverflow.com/a/9399907/1579481
-function extend() {
-    var options, name, src, copy, copyIsArray, clone,
-        target = arguments[0] || {},
-        i = 1,
-        length = arguments.length,
-        deep = false,
-        toString = Object.prototype.toString,
-        hasOwn = Object.prototype.hasOwnProperty,
-        push = Array.prototype.push,
-        slice = Array.prototype.slice,
-        trim = String.prototype.trim,
-        indexOf = Array.prototype.indexOf,
-        class2type = {
-            '[object Boolean]': 'boolean',
-            '[object Number]': 'number',
-            '[object String]': 'string',
-            '[object Function]': 'function',
-            '[object Array]': 'array',
-            '[object Date]': 'date',
-            '[object RegExp]': 'regexp',
-            '[object Object]': 'object'
-        },
-        jQuery = {
-            isFunction: function(obj) {
-                return jQuery.type(obj) === 'function';
-            },
-            isArray: Array.isArray || function(obj) {
-                return jQuery.type(obj) === 'array';
-            },
-            isWindow: function(obj) {
-                return obj != null && obj == obj.window;
-            },
-            isNumeric: function(obj) {
-                return !isNaN(parseFloat(obj)) && isFinite(obj);
-            },
-            type: function(obj) {
-                return obj == null ? String(obj) : class2type[toString.call(obj)] || 'object';
-            },
-            isPlainObject: function(obj) {
-                if (!obj || jQuery.type(obj) !== 'object' || obj.nodeType) {
-                    return false;
-                }
-                try {
-                    if (obj.constructor && !hasOwn.call(obj, 'constructor') && !hasOwn.call(obj.constructor.prototype, 'isPrototypeOf')) {
-                        return false;
-                    }
-                } catch ( e ) {
-                    return false;
-                }
-                var key;
-                for (key in obj) {}
-                return key === undefined || hasOwn.call(obj, key);
-            }
-        };
-    if (typeof target === 'boolean') {
-        deep = target;
-        target = arguments[1] || {};
-        i = 2;
-    }
-    if (typeof target !== 'object' && !jQuery.isFunction(target)) {
-        target = {};
-    }
-    if (length === i) {
-        target = this;
-        --i;
-    }
-    for (i; i < length; i++) {
-        if ((options = arguments[i]) != null) {
-            for (name in options) {
-                src = target[name];
-                copy = options[name];
-                if (target === copy) {
-                    continue;
-                }
-                if (deep && copy && (jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)))) {
-                    if (copyIsArray) {
-                        copyIsArray = false;
-                        clone = src && jQuery.isArray(src) ? src : [];
-                    } else {
-                        clone = src && jQuery.isPlainObject(src) ? src : {};
-                    }
-                    // WARNING: RECURSION
-                    target[name] = extend(deep, clone, copy);
-                } else if (copy !== undefined) {
-                    target[name] = copy;
-                }
-            }
-        }
-    }
-    return target;
-}
