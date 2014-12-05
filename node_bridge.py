d451fb5c9148f2f9dd4abb98acfb2f93192fb539
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2014-12-05 15:00:24 +0200

    Adds customizable node path #19

--- node_bridge.py
+++ node_bridge.py
@@ -1,18 +1,22 @@
 import os
 import platform
 from subprocess import Popen, PIPE
+import sublime
 
 IS_OSX = platform.system() == 'Darwin'
 IS_WINDOWS = platform.system() == 'Windows'
+SETTINGS_FILE = 'jsfmt.sublime-settings'
 
 def node_bridge(data, bin, cdir, args=[]):
+	settings = sublime.load_settings(SETTINGS_FILE)
+	print(settings.get('node-path'))
 	env = None
 	if IS_OSX:
 		# GUI apps in OS X doesn't contain .bashrc/.zshrc set paths
 		env = os.environ.copy()
 		env['PATH'] += ':/usr/local/bin'
 	try:
-		p = Popen(['node', bin] + args,
+		p = Popen([settings.get('node-path', 'node'), bin] + args,
 			stdout=PIPE, stdin=PIPE, stderr=PIPE,
 			cwd=cdir, env=env, shell=IS_WINDOWS)
 	except OSError:
